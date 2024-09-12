4835a6bafef39536107b1033e6ab8f70bd95ae0e
Savina Daria <savina.dariia@chnu.edu.ua>
2024-09-12 13:33:54 +0300

    initial

---
+++ BookService/src/test/java/BookStore/BookService/TestcontainersConfiguration.java
@@ -0,0 +1,8 @@
+package BookStore.BookService;
+
+import org.springframework.boot.test.context.TestConfiguration;
+
+@TestConfiguration(proxyBeanMethods = false)
+class TestcontainersConfiguration {
+
+}
